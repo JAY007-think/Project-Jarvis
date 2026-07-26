95a886cf4b3e565e120a4dd7bac999e43cf30085
JAY SONI <jaysoni9549@gmail.com>
2026-07-26 17:26:10 +0530

    main.py updated

--- src/main.py
+++ src/main.py
@@ -15,51 +15,6 @@ from skills import news
     
 NewsApi = os.getenv("NEWS_API_KEY")
 
-<<<<<<< HEAD
-=======
-# speak function
-def speak_old(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
-def speak(text):
-    tts = gTTS(text)
-    tts.save('temp.mp3')
-
-    # Initialize pygame mixer
-    pygame.mixer.init()
-
-    # Load the mp3 file
-    pygame.mixer.music.load('temp.mp3')
-
-    # play the mp3 file
-    pygame.mixer.music.play()
-
-    # keep the program running until the music stops playing
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-    
-    pygame.mixer.music.unload()
-    os.remove("temp.mp3")
-    
-def AiProcess(command):
-    from openai import OpenAI
-
-    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
-    completion = client.chat.completions.create(
-    model="gpt-5-nano",
-    messages=[
-        {"role":"system","content":"You are a virtual assistant named jarvis skilled in general task like alexa and google cloud,but you always speak hindi and briefly, give short and better responses, you are made by a smart software devloper name JAY . you always give amazing answer which shocked peoples, you are highly trained assistant"},
-        {"role":"user","content":command}
-        ]
-    )
-
-    return completion.choices[0].message.content
-
-# command processor
->>>>>>> 71c1e1ab3dfa5e1e03b05f2be3c64c6d50d7f446
 def processCommand(command):
     if "open google" in command:
         web.open_google()
